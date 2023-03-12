d3bec92768292f2a648a473410840764ee196808
Robin Lu <wwng@2333.me>
2023-03-12 20:57:56 +0800

    main: add LED control

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main(void)
 {
+    P_SW2 |= 0x80;
+    COMEN = 0x83;
+    SEGENH = 0xFF;
+    LEDCKS = 7;
+    COM0_DC_L = 0xFF;
+    COM0_DC_H = 0xFF;
+    LEDCTRL  = 0x80;
+
     UartInit();
     UartSendStr("Uart Test !\r\n");
     while(1);
